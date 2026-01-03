d0675bae537132d9afbc1f9a862d05a19fb0296d
Sathyan S S <sssathyan2004@gmail.com>
2026-01-03 15:05:22 +0530

    Update simple_dual_port_ram.v

--- 05. Sequential Circuits/07. Memory Circuits/03. Dual Port RAM/01. Simple Dual Port RAM/simple_dual_port_ram.v
+++ 05. Sequential Circuits/07. Memory Circuits/03. Dual Port RAM/01. Simple Dual Port RAM/simple_dual_port_ram.v
@@ -1,19 +1,19 @@
 module simple_dual_port_ram #(parameter length=4, locations=8)(
 	input clk,rw,
-	input [$clog2(locations)-1:0] w_addr,
-	input [$clog2(locations)-1:0] r_addr,
+	input [$clog2(locations)-1:0] w_addr, // Write Address
+	input [$clog2(locations)-1:0] r_addr, // Read Address
 	input [length-1:0] indata,
 	output reg [length-1:0] outdata
 );
 reg [length-1:0] memory [locations-1:0];
 always @(posedge clk) begin
 	if(rw) begin
-		memory[w_addr] <= indata; //Synchronous Write
+		memory[w_addr] <= indata; // Synchronous Write
 	end
 end
 always @(*) begin
 	if(!rw) begin
-		outdata = memory[r_addr]; // Asynchronous Write
+		outdata = memory[r_addr]; // Asynchronous Read
 	end
 end
 endmodule
