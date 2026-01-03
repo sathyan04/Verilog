a61f8809b3b4e2cff77e5ce78b6192bc3728f270
Sathyan S S <sssathyan2004@gmail.com>
2026-01-03 13:09:05 +0530

    Add files via upload

---
+++ 05. Sequential Circuits/07. Memory Circuits/02. Single Port RAM/01. Single Port RAM (Sync Read)/par_ram.v
@@ -0,0 +1,16 @@
+module ram_parameterized #(parameter length=4, locations=8)(
+	input clk,rw,
+	input [$clog2(locations)-1:0] addr,
+	input [length-1:0] indata,
+	output reg [length-1:0] outdata
+);
+
+reg [length-1:0] memory [locations-1:0];
+
+always @(posedge clk) begin
+	if(rw) begin
+		memory[addr] <= indata; //Synchronous Write
+	end
+	outdata = memory[addr]; // Synchronous Write
+end
+endmodule
