34f0925d24e1dbfda3f6e7ce945576a7b966e6c6
Sathyan S S <sssathyan2004@gmail.com>
2026-01-03 10:58:07 +0530

    Add files via upload

---
+++ 05. Sequential Circuits/07. Memory Circuits/01. Simple RAM/01. RAM - Synchronous Read/simple_ram.v
@@ -0,0 +1,14 @@
+module ram(
+	input clk,rw,
+	input [3:0] addr,
+	input [7:0] indata,
+	output reg [7:0] outdata
+);
+reg [7:0] memory [15:0]; // 16 * 8 = 128 bits / 16 bytes of memory
+always @(posedge clk) begin
+	if(rw) begin
+		memory[addr] <= indata; //Synchronous Write
+	end
+	outdata<=memory[addr]; //Synchronous Read
+end
+endmodule
