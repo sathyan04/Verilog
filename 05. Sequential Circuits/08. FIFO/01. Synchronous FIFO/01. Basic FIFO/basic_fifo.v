ffa127cc4f7807915f785ddf6f43fa50019b12b5
Sathyan S S <sssathyan2004@gmail.com>
2026-01-06 10:47:20 +0530

    Create basic_fifo.v

---
+++ 05. Sequential Circuits/08. FIFO/01. Synchronous FIFO/01. Basic FIFO/basic_fifo.v
@@ -0,0 +1,26 @@
+module fifo_basic(
+	input clk, rst, w_en, r_en, 
+	input wire [7:0] in_data,
+	output reg [7:0] out_data
+);
+reg [7:0] memory [3:0];
+reg [1:0] w_ptr,r_ptr;
+
+always @(posedge clk) begin
+	if(rst) begin
+		w_ptr=0;
+		r_ptr=0;
+		out_data=0;
+	end
+	else begin
+		if(w_en) begin
+			memory[w_ptr]<=in_data;
+			w_ptr<=w_ptr+1;
+		end
+		else if (r_en) begin
+			out_data<=memory[r_ptr];
+			r_ptr<=r_ptr+1;
+		end
+	end
+end
+endmodule	
