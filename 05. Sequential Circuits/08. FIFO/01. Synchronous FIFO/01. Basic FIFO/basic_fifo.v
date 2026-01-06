0066ca9d23a15c57c9586e6605a2e926cb0732fb
Sathyan S S <sssathyan2004@gmail.com>
2026-01-06 17:50:38 +0530

    Update basic_fifo.v

--- 05. Sequential Circuits/08. FIFO/01. Synchronous FIFO/01. Basic FIFO/basic_fifo.v
+++ 05. Sequential Circuits/08. FIFO/01. Synchronous FIFO/01. Basic FIFO/basic_fifo.v
@@ -8,9 +8,9 @@ reg [1:0] w_ptr,r_ptr;
 
 always @(posedge clk) begin
 	if(rst) begin
-		w_ptr=0;
-		r_ptr=0;
-		out_data=0;
+		w_ptr<=0;
+		r_ptr<=0;
+		out_data<=0;
 	end
 	else begin
 		if(w_en) begin
