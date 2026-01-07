767ea60733b512aacb5dfe535c17e83148ae950f
Sathyan S S <sssathyan2004@gmail.com>
2026-01-07 19:20:00 +0530

    Folders have been structured....

--- 02. Gate Logics/01. AND gate/and_gate_tb.v
+++ 02. Gate Logics/01. AND gate/and_gate_tb.v
@@ -1,18 +1,18 @@
-`timescale 1ns / 1ps
-
 module and_gate_tb;
 
   reg a, b;
   wire y;
 
-  and_gate_ uut (.a(a), .b(b), .y(y));
+  and_gate_ dut (.a(a), .b(b), .y(y));
 
   initial begin
-    a = 0; b = 0; #10;
-    a = 0; b = 1; #10;
-    a = 1; b = 0; #10;
-    a = 1; b = 1; #10;
-    $finish;
+	  $dumpfile("andgkt.vcd");
+	  $dumpvars(0);
+	  a = 0; b = 0; #10;
+	  a = 0; b = 1; #10;
+	  a = 1; b = 0; #10;
+	  a = 1; b = 1; #10;
+	  $finish;
   end
 
 endmodule
