ad96025637036680c79118761063548dd2f673eb
Sathyan S S <sssathyan2004@gmail.com>
2026-01-29 09:53:09 +0530

    Folder have been structured

--- 06. HDL-Bits Solutions/02. Verilog Language/03. Modules: Hierarchy/01_Modules.v
+++
@@ -1,3 +0,0 @@
-module top_module ( input a, input b, output out );
-    mod_a dut(a,b,out);
-endmodule
--- 06. HDL-Bits Solutions/03. Circuits/01. Combinational Logic/01. Basic Gates/11_Simple_circuit_B.v
+++
@@ -1,3 +0,0 @@
-module top_module ( input x, input y, output z );
-    xnor(z,x,y);
-endmodule
