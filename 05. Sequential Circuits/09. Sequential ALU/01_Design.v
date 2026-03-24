6dd88bf6f8d793279e2aac92134617abe90639de
Sathyan S S <sssathyan2004@gmail.com>
2026-03-24 18:04:45 +0530

    Update 01_Design.v

--- 05. Sequential Circuits/09. Sequential ALU/01_Design.v
+++ 05. Sequential Circuits/09. Sequential ALU/01_Design.v
@@ -1,59 +1,93 @@
-module seq_alu #(parameter data_width = 4, parameter op_size = 1)(
-  input clk, rst,
-  input [op_size-1:0] op,
-  input [data_width-1:0] a, b,
+module seq_alu #(parameter data_width = 4)(
+  input clk, 
+  input rst,
+  input [3] op,
+  input [data_width-1:0] a, 
+  input [data_width-1:0] b,
   output reg [data_width-1:0] out,
-  output reg z, c, n, v, b
+  //Flags
+  output reg zero,
+  output reg carry, 
+  output reg negative,
+  output reg overflow, 
+  output reg borrow, 
+  output reg parity
 );
-  
-  /* 
-  Flags: 
-  	z - Zero
-    c - Carry
-    n - Negative
-    v - Overflow
-    b - Borrow
-  */
-
-  parameter [op_size-1:0] addition=0, 
-  subtraction=1;
-  
-  reg [data_width:0] temp; // carry handling
-  
+    
   always @(posedge clk) begin
     
     if(rst) begin
-      z<=0;
-      c<=0;
-      n<=0;
-      v<=0;
-      b<=0;
-      out<=0;
-      temp<=0;
+      zero			<=	0;
+      carry			<=	0;
+      overflow		<=	0;
+      borrow		<=	0;
+      out			<=	0;
     end
     
     else begin
+    
       case(op)
         
-        addition: begin
-          temp<=a+b;
-          out<=temp[data_width-1:0];
-          c<=temp[data_width];
-          v<=(a[data_width-1]==b[data_width-1]) && (temp[data_width-1]!=a[data_width-1]);
+        //Arithmetic Operations
+        
+        //Addition
+        3'd0: begin
+          {carry, out} <= a+b;
+          overflow <= (a[data_width-1]==b[data_width-1]) && (out[data_width-1]!=a[data_width-1]);
         end
         
-        subtraction: begin
-          temp<=a-b;
-          out<=temp[data_width-1:0];
-          b<=temp[data_width];
-          v<=(a[data_width-1]!=b[data_width-1]) && (temp[data_width-1]!=a[data_width-1]);
+        //Subtraction
+        3'd1: begin
+          
+          if(a>b) begin
+            {carry, out} <= a-b;
+          end
+          else if(a<b) begin
+            {carry, out} <= b-a;
+          end
+          else begin
+            carry <= 0;
+            out <= 0;
+          end
+          
+          overflow <= (a[data_width-1]!=b[data_width-1]) && (out[data_width-1]!=a[data_width-1]);
+          borrow <= ~carry;
         end
         
+        //Multiplication
+        3'd2: begin
+          {carry, out} <= a*b;
+        end
+        
+        //Division
+        3'd3: begin
+          out <= a/b;
+        end
+        
+        //Logical Operations
+        
+        //AND Gate
+        3'd4: out <= a&b;
+        
+        //OR Gate
+        3'd5: out <= a|b;
+        
+        //XOR Gate
+        3'd6: out <= a^b;
+        
+        //XNOR Gate
+        3'd7: out <= ~(a^b);
+        
+        default: out <= 3'bx;
+      
       endcase
       
-      z<=(out == 0);
-      n<=out[data_width-1];
+      zero <= (out == 0);
+      negative <= (out[data_width-1] == 1);
+      parity <= ~(^out);
+    
     end
+  
   end
   
 endmodule
